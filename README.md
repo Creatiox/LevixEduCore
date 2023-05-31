6d36f4beeffce4e571884a9c5be183fffcf6a36d
Maximiliano Ramirez <m.ramirez.bravo.94@gmail.com>
2023-05-31 11:51:43 -0400

    feat!: Versión inicial (#1)

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
+![Levix Edu Core](.img/logo.png)
+
 <h1 align="center">
-  <a><img src=".img/logo.png" alt="Logo" width="400"></a>
-  <br>
-  Título
+  LevixEduCore
 </h1>
 
 <p align="center">
-  <b>Subtítulo</b>
+  <b>Librería base para placas Levix Edu con Levix Edu Core</b>
 </p>
 
 ---
@@ -13,23 +13,120 @@
 ## Tabla de contenido <!-- omit in toc -->
 - [Descripción](#descripción)
 - [Uso de librería](#uso-de-librería)
-  - [Inicialización](#inicialización)
+  - [Clase heredada (otra librería de alguna placa de desarrollo Levix Edu)](#clase-heredada-otra-librería-de-alguna-placa-de-desarrollo-levix-edu)
+  - [Terminal (monitor serial)](#terminal-monitor-serial)
+  - [WiFi](#wifi)
+  - [Control energía](#control-energía)
 
 ---
 
 ## Descripción
-Esta librería permite...
+Esta pequeña librería es la base para otras librerías de placas de desarrollos **Levix Edu**. Es una
+abstracción que incluye un control asíncrono del *WiFi* para facilitar su uso, y también algunas
+opciones de energía para el reinicio y el modo *deep-sleep*.
 
 ## Uso de librería
-A continuación, se muestran algunos ejemplos...
+Esta librería no se puede utilizar sola, ya que es una clase abstracta. Por ende, necesita ser
+implementada en otra librería para placa de desarrollo, como
+[LevixEduCoreDev]([https://](https://github.com/Creatiox/LevixEduCoreDev)).
 
-### Inicialización
+Sin embargo, los métodos base se pueden utilizar de la siguiente forma:
+
+### Clase heredada (otra librería de alguna placa de desarrollo Levix Edu)
 ```cpp
+// Lo mínimo que necesita otra clase que derive de LevixEduCore
+class LevixEduABC : public LevixEduCore {
+  public:
+  LevixEduABC() {}
+
+  void init() {
+    // Inicializar periféricos, variables, etc.
+  }
+};
+
 // Crear objeto
-Example example;
+LevixEduABC levix;
 
 void setup() {
-  // Inicializar
-  example.begin();
+  // Inicializar Levix
+  levix.init();
+}
+```
+
+### Terminal (monitor serial)
+```cpp
+void setup() {
+  // Inicializar Levix
+  levix.init();
+
+  // Utilizar levix.terminal() para acceder al monitor serial
+  levix.terminal().println("Hola desde Levix");
+}
+```
+
+### WiFi
+```cpp
+#define SSID "ssid"
+#define PASS "password"
+
+void onConnect() {
+  levix.terminal().println("WiFi conectado!");
+}
+
+void onDisconnect() {
+  levix.terminal().println("WiFi desconectado!");
+}
+
+void setup() {
+  // Inicializar Levix
+  levix.init();
+  
+  // Establecer funciones que se ejecuten cuando el WiFi se conecte y desconecte de la red
+  levix.wifiOnConnected(onConnect);
+  levix.wifiOnDisconnected(onDisconnect);
+
+  // Establecer IP estática
+  levix.wifiSetStaticIP(IPAddress(192, 168, 0, 100),
+                        IPAddress(192, 168, 0, 1),
+                        IPAddress(255, 255, 255, 0),
+                        IPAddress(8, 8, 8, 8),
+                        IPAddress(8, 8, 4, 4));
+
+  // Establecer IP dinámica
+  // levix.wifiSetStaticIP();
+
+  // Habilitar WiFi y conectarse a la red
+  levix.wifiEnable(SSID, PASSWORD);
+  
+  // Deshabilitar WiFi
+  // levix.wifiDisable();
+}
+
+void loop() {
+  if (levix.isWifiConnected()) {
+    // Hacer alguna operación si el wifi se encuentra conectado
+  }
+}
+```
+
+### Control energía
+```cpp
+void setup() {
+  // Inicializar Levix
+  levix.init();
+
+  // Obtener razones de reinicio y salida modo sleep
+  // Valores se encuentran en los enumeradores "esp_*_t"
+  esp_reset_reason_t reset        = levix.powerGetResetReason();
+  esp_sleep_wakeup_cause_t wakeup = levix.powerGetWakeupReason();
+
+  // Reiniciar Levix
+  // levix.restart();
+}
+
+void loop() {
+  // Esperar 5 segundos y luego activar modo sleep por 5 segundos
+  delay(5000);
+  levix.powerTimedDeepSleep(5);
 }
 ```
---
+++ src/LevixEduCore.h
@@ -0,0 +1,150 @@
+/**
+ * SPDX-FileCopyrightText: 2023 Creatiox
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef LEVIX_EDU_CORE_H
+#define LEVIX_EDU_CORE_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+
+/// @brief Prototipo de función para métodos wifiOnConnected() y wifiOnDisconnected().
+typedef void (*LevixWiFiCallback)();
+
+class LevixEduCore {
+  public:
+  LevixEduCore();
+
+  virtual void init() = 0;
+
+  /**
+   * @brief Obtener la referencia a la UART del monitor serie. Ej: levix.terminal().print().
+   *
+   * @return HardwareSerial& UART Terminal
+   */
+  HardwareSerial& terminal();
+
+  /**
+   * @brief Reiniciar Levix.
+   */
+  virtual void powerRestart();
+
+  /**
+   * @brief Obtener motivo del último reinicio.
+   *
+   * @return esp_reset_reason_t Motivo de reinicio
+   */
+  virtual esp_reset_reason_t powerGetResetReason();
+
+  /**
+   * @brief Obtener motivo de haber despertado del modo suspensión.
+   *
+   * @return esp_sleep_wakeup_cause_t Motivo despertar del modo suspensión
+   */
+  virtual esp_sleep_wakeup_cause_t powerGetWakeupReason();
+
+  /**
+   * @brief Dormir Levix por un período de tiempo determinado. Apaga todos los periféricos
+   * innecesarios para ahorrar el máximo de energía posible durante este modo.
+   * 
+   * @param seconds Segundos duración modo suspensión
+   * @return true OK (esta función no retorna)
+   * @return false No se pudo dormir el dispositivo
+   */
+  virtual bool powerTimedDeepSleep(uint32_t seconds);
+
+  /**
+   * @brief Habilitar WiFi. Cada 10s intenta conectarse a la red establecida. Solamente se puede
+   * utilizar redes de 2.4GHz.
+   *
+   * @param ssid SSID de la red
+   * @param password Contraseña de la red
+   * @return true OK
+   * @return false No se pudo habilitar el periférico o ya se encuentra encendido
+   */
+  virtual bool wifiEnable(const char* ssid, const char* password);
+
+  /**
+   * @brief Establecer una IP estática para el WiFi. Permite cambiar la configuración en cualquier
+   * momento.
+   *
+   * @param ip IP local
+   * @param gateway IP gateway
+   * @param mask Máscara de red
+   * @param dns1 [Opcional] DNS 1
+   * @param dns2 [Opcional] DNS 2
+   * @return true OK
+   * @return false No se pudo cambiar la configuración
+   */
+  virtual bool wifiSetStaticIP(IPAddress ip, IPAddress gateway, IPAddress mask,
+                               IPAddress dns1 = (uint32_t)0, IPAddress dns2 = (uint32_t)0);
+
+  /**
+   * @brief Establecer IP dinámica para el WiFi. Busca una IP disponible
+   * automáticamente.
+   *
+   * @return true OK
+   * @return false No se pudo cambiar la configuración
+   */
+  virtual bool wifiSetDynamicIP();
+
+  /**
+   * @brief Deshabilitar WiFi. Se desconecta de la red y apaga el periférico.
+   *
+   * @return true OK
+   * @return false No se pudo apagar el periférico o ya se encuentra
+   * apagado
+   */
+  virtual bool wifiDisable();
+
+  /**
+   * @brief Establecer función externa para ejecutarla cuando Levix se conecte a una red WiFi.
+   *
+   * @param connected_function Función
+   */
+  virtual void wifiOnConnected(LevixWiFiCallback connected_function);
+
+  /**
+   * @brief Establecer función externa para ejecutarla cuando Levix se desconecte de una red WiFi.
+   *
+   * @param connected_function Función
+   */
+  virtual void wifiOnDisconnected(LevixWiFiCallback disconnected_function);
+
+  /**
+   * @brief Consultar si el WiFi se encuentra conectado a la red.
+   *
+   * @return true Conectado
+   * @return false Desconectado
+   */
+  virtual bool isWifiConnected();
+
+  protected:
+  HardwareSerial& _uart_terminal;
+  esp_reset_reason_t _last_reset_reason;
+  esp_sleep_wakeup_cause_t _last_wakeup_reason;
+
+  virtual void wifiInit();
+  static bool _wifi_enabled;
+  static const char* _wifi_ssid;
+  static const char* _wifi_pass;
+  static LevixWiFiCallback _wifi_connected_cb;
+  static LevixWiFiCallback _wifi_disconnected_cb;
+  static bool _wifi_connected;
+
+  static void wifiCallback(arduino_event_id_t event);
+
+  static TimerHandle_t _wifi_timer_handle;
+  static StaticTimer_t _wifi_timer_buffer;
+  static void wifiTimer(void* pvParameters);
+
+  static const uint16_t _wifi_task_size;
+  static TaskHandle_t _wifi_task_handle;
+  static StackType_t _wifi_task_stack[];
+  static StaticTask_t _wifi_task_static_task;
+  static void wifiTask(void* pvParameters);
+};
+
+#endif // LEVIX_EDU_CORE_H
